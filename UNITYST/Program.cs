93a031034cede9f27da33841c221eb95a589b1e6
PC <PC@DESKTOP-VBIT3JE>
2024-09-02 16:18:03 +0900

    초기 커밋.

---
+++ UNITYST/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; // == include
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UNITYST // 개념의 분류, 굳이 필요는 x
+{
+    class Program //( == 객체, 식별자 )
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
